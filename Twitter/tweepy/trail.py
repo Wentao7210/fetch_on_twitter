352a16886de77b31d246d23b148a64a9bcf57201
Wentao7210 <wentaosun.sh@gmail.com>
2023-07-09 19:43:32 +0100

    update

--- Twitter/tweepy/trail.py
+++ Twitter/tweepy/trail.py
@@ -1,9 +1,7 @@
 import tweepy
-api_key = 'owjYr5mvQvHv30zyUom7Zb8TT'
-api_key_secret = 'ZclerRcrtS5BMxqWVVO9mTvD9xYrofVqco28syvTcXxKXJA4rg'
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAAO%2FNogEAAAAAYrQBm8t5hmludDv7TePWlh1nVIs%3DyG9bVxkrJONCEOPjEBIDcBqJ6H5ChfTomPfdqnUyHXfbinEUfL'
 
-client = tweepy.Client("AAAAAAAAAAAAAAAAAAAAAO%2FNogEAAAAAYrQBm8t5hmludDv7TePWlh1nVIs%3DyG9bVxkrJONCEOPjEBIDcBqJ6H5ChfTomPfdqnUyHXfbinEUfL")
+
+client = tweepy.Client("BEARER_TOKEN")
 
 query = 'from:elonmusk -is:retweet'
 tweets = client.search_recent_tweets(query=query, max_results=100)
